b60b824f37f719dc92930e4aaab446aea28f16c8
greshowy <jakub_przy@wp.pl>
2025-12-14 22:28:54 +0100

    create page list ticket for it

--- app/routes/dashboardRout.js
+++ app/routes/dashboardRout.js
@@ -9,31 +9,36 @@ router.get('/dashboard', (req, res) => {
 
 router.get('/dashboard/it', (req, res) => {
     res.render('pages/dashboard/it', {
-        tittle: 'Dział IT'
+        tittle: 'Dział IT',
+        layout: 'layouts/dashboardLayouts'
     });
 });
 
 router.get('/dashboard/elektryczny', (req, res) => {
     res.render('pages/dashboard/electrical', {
-        tittle: 'Dział Elektryczny'
+        tittle: 'Dział Elektryczny',
+        layout: 'layouts/dashboardLayouts'
     });
 });
 
 router.get('/dashboard/aparatura-medyczna', (req, res) => {
     res.render('pages/dashboard/medicalEquipment', {
-        tittle: 'Dział Aparatury Medycznej'
+        tittle: 'Dział Aparatury Medycznej',
+        layout: 'layouts/dashboardLayouts'
     });
 });
 
 router.get('/dashboard/budowlany', (req, res) => {
     res.render('pages/dashboard/construction', {
-        tittle: 'Dział Budowlany'
+        tittle: 'Dział Budowlany',
+        layout: 'layouts/dashboardLayouts'
     });
 });
 
 router.get('/dashboard/cyberbezpieczenstwo', (req, res) => {
     res.render('pages/dashboard/cybersecurity', {
-        tittle: 'Dział Cyberbezpieczeństwa'
+        tittle: 'Dział Cyberbezpieczeństwa',
+        layout: 'layouts/dashboardLayouts'
     });
 });
 
